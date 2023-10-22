53bd52f69a29a33ff55c8f2e5bdb2ee026d3acd0
lgraul87 <creatupaginawebparatuactividad@gmail.com>
2023-10-22 02:15:21 +0200

    Seo test 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Meta } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -6,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'creatupaginawebparatuactividad';
+
+  constructor(private meta: Meta) { }
+  
+  ngOnInit(): void {
+    this.meta.addTag({ name: 'description', content: 'Este es un artículo sobre optimización SEO en Angular.' });
+    this.meta.addTag({ name: 'keywords', content: 'Angular, SEO, meta etiquetas, optimización' });
+  }
 }
