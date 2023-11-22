d53430f9a68bb439eb7e4de82466df3d29c2e402
lgraul87 <creatupaginawebparatuactividad@gmail.com>
2023-11-22 01:13:29 +0100

    added nlinks

--- src/app/home/body/body.component.ts
+++ src/app/home/body/body.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-body',
@@ -7,4 +8,9 @@ import { Component } from '@angular/core';
 })
 export class BodyComponent {
 
+  constructor(private router: Router) { }
+
+  navigateToContacto() {
+    this.router.navigateByUrl('contacto');
+  }
 }
--- src/app/shared/menu/component/menu.component.ts
+++ src/app/shared/menu/component/menu.component.ts
@@ -30,56 +30,46 @@ export class MenuComponent {
   navigateToPageSelected(idPage: String) {
     switch (idPage) {
       case "Home":
-        console.log("Home");
+        this.router.navigateByUrl('');
         break;
 
       case "Quiénes somos":
-        console.log("Quiénes somos");
         this.router.navigateByUrl('quienes-somos');
         break;
 
       case "Qué Hacemos ...":
-        console.log("Qué Hacemos ...");
         break;
 
       case "Tu página web a medida":
-        console.log("Tu página web a medida");
         this.router.navigateByUrl('tu-pagina-web-a-medida');
         break;
 
       case "SEO, posicionamos tu web":
-        console.log("SEO, posicionamos tu web");
         this.router.navigateByUrl('seo-posicionamos-tu-web');
 
         break;
 
       case "Dominio personalizado":
-        console.log("Dominio personalizado");
         this.router.navigateByUrl('dominio-personalizado');
         break;
 
       case "Evolutivos de tu web":
-        console.log("Evolutivos de tu web");
         this.router.navigateByUrl('evolutivos-de-tu-web');
         break;
 
       case "Soporte y mantenimiento":
-        console.log("Soporte y mantenimiento");
         this.router.navigateByUrl('soporte-y-mantenimiento');
         break;
 
       case "Portafolio":
-        console.log("Portafolio");
         this.router.navigateByUrl('portafolio');
         break;
 
       case "Blog":
-        console.log("Blog");
         this.router.navigateByUrl('blog');
         break;
 
       case "Contacto":
-        console.log("Contacto");
         this.router.navigateByUrl('contacto');
         break;
 
